06d984ca2464e344f71f8bdb934e14fa3e33fdc4
Hugo de la Torre Gil <htorgil850@g.educaand.es>
2025-11-25 13:15:50 +0100

    25/11/2025 - 13:15

---
+++ src/utils/FileManager.java
@@ -0,0 +1,54 @@
+package utils;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class FileManager {
+
+    public ArrayList<String> leerCSV(String ruta){
+        ArrayList<String> lineas = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(ruta))){
+            String linea = br.readLine();
+            while(linea != null){
+                lineas.add(linea);
+                linea = br.readLine();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lineas;
+    }
+
+    public void escribirCSV(String ruta, ArrayList<String> listaLineas){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(ruta))){
+            for(String linea : listaLineas){
+                bw.write(linea);
+                bw.newLine();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> leerDat(String ruta){
+        ArrayList<String> lineas = new ArrayList<>();
+        try(ObjectInputStream flujoBytesEntrada = new ObjectInputStream(new FileInputStream(ruta)))
+        {
+            while(true){
+                try{
+                    Object obj = flujoBytesEntrada.readObject();
+                    String linea = String.valueOf(obj);
+                    lineas.add(linea);
+                } catch (EOFException e) {
+                    break;
+                }
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lineas;
+    }
+}
